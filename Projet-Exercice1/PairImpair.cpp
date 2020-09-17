115268a8db9c37e602c29fd1664beda09019b670
Karine Moreau <70488225+kmoreau-cstj@users.noreply.github.com>
2020-09-17 09:32:50 -0400

    Bon travail
    Le programme fonctionne parfaitement ! Manque un commentaire et complète le plan de tests

--- Projet-Exercice1/PairImpair.cpp
+++ Projet-Exercice1/PairImpair.cpp
@@ -1,4 +1,4 @@
-//But: Créer un programme qui lit en entier et détermine s'il s'agit d'un nombre impair ou pair
+//But: CrÃ©er un programme qui lit en entier et dÃ©termine s'il s'agit d'un nombre impair ou pair
 //Auteur: Marc-Olivier Parisien
 //Date: 11 septembre 2020
 
@@ -8,15 +8,16 @@ void main()
 {
 	setlocale(LC_ALL, ""); //Pour convertir correctement les accents
 
-	//Nom de la variable que l'utilisateur entrera lorsque demandé; la valeur y sera stockée
+	//Nom de la variable que l'utilisateur entrera lorsque demandÃ©; la valeur y sera stockÃ©e
 	int nb;
 	
-	 // On invite l'utilisateur a entrer une valeur pair ou impair
+	 // FR : On invite l'utilisateur a entrer une valeur pair ou impair
 	std::cout << "Veuillez entrer un nombre entier. ";
 
-	 // Le programme stock la valeur dans la variable initialiser par l'utilisateur
+	 // FR : Le programme stock la valeur dans la variable initialiser par l'utilisateur
 	std::cin >> nb;
 	 
+	// KM : Manque un commentaire ici pour expliquer pourquoi tu fais ce calcul
 	if(nb % 2 == 0)
 	{
 		std::cout << nb << " est un nombre pair. ";
@@ -28,8 +29,9 @@ void main()
 
 }
 //Plan de test
+// KM : Ajoute des valeurs nÃ©gatives
 /*
-	nb		résultat
+	nb		rÃ©sultat
 	10		10 est un nombre pair.
 	1		1 est un nombre impair.
 	0		0 est un nombre pair.
